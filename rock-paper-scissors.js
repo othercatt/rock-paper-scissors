832f0a28d02c7ce401225a417da7d650f677613f
othercatt <othercatt@gmail.com>
2022-04-05 01:15:40 -0500

    Clean file and remove old notes

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,7 @@
+// GAME FUNCTIONS //
 
 function computerPlay() {
-  let selector = Math.floor(Math.random()*3); // returns 0, 1, or 2
+  let selector = Math.floor(Math.random()*3);
   if (selector == 0) {
     computerSelection = 'Rock';
   } else if (selector == 1) {
@@ -8,7 +9,6 @@ function computerPlay() {
   } else if (selector == 2) {
     computerSelection = 'Scissors';
   }
-  console.log('Computer: ' + computerSelection); // remove when finished
   return computerSelection;
 };
 
@@ -46,13 +46,9 @@ function compareSelections(playerSelection, computerSelection) {
 
 
 function playRound(playerSelection, computerSelection) {
-  // Compare choices and determine outcome
   let outcome = compareSelections(playerSelection, computerSelection);
-
-  // Update the scoreboard
   updateScore(playerSelection, computerSelection, outcome);
 
-  // Check if either score has reach 5 - if so, announce winner and end
   if (playerWins == 5) {
     resultText.textContent = ("Congrats! You were the first to 5 points!");
     endGame();
@@ -60,13 +56,12 @@ function playRound(playerSelection, computerSelection) {
     resultText.textContent = ("Sorry, the computer reached 5 and wins this time!");
     endGame();
   }
-  // Return the outcome value for later use
+  
   return outcome;
 };
 
 
 function updateScore(playerSelection, computerSelection, outcome) {
-  // Increase counter by 1 and update scoreboard
   let playerCounter = document.getElementById('player-counter');
   let computerCounter = document.getElementById('computer-counter');
   
@@ -85,12 +80,10 @@ function updateScore(playerSelection, computerSelection, outcome) {
 
 
 function endGame() {
-  // Disable player selection buttons
   document.getElementById('rock').disabled = true;
   document.getElementById('paper').disabled = true;
   document.getElementById('scissors').disabled = true;
 
-  // Create a button to reset counters and start a new game
   const newGameBtn = document.createElement('button');
   newGameBtn.id = 'new-game-button';
   newGameBtn.innerHTML = 'New Game';
@@ -99,7 +92,7 @@ function endGame() {
   newGameBtn.addEventListener('click', () => {
     newGameBtn.remove();
     resetScoreboard();
-    startNewGame();
+    resetChoiceButtons();
   });
 };
 
@@ -118,16 +111,16 @@ function resetScoreboard() {
  };
 
 
-function startNewGame() {
+function resetChoiceButtons() {
   document.getElementById('rock').disabled = false;
   document.getElementById('paper').disabled = false;
   document.getElementById('scissors').disabled = false;
-  
-  console.log('New game button clicked');
 };
 
 
 
+
+// GAME STATE
 
 // State of the game upon starting
 let playerWins = 0;
@@ -135,24 +128,21 @@ let computerWins = 0;
 let buttons = document.getElementById('selection-buttons');
 let resultText = document.getElementById('match-result');
 
-// Processes to run during a five-point match
+// Player choice button event listeners - take choice and play one round
 const rockBtn = document.getElementById('rock');
 rockBtn.addEventListener('click', () => {
   console.log('Clicked rock');
   let result = (playRound('Rock', computerPlay()));
-  console.log(result);
 });
 
 const paperBtn = document.getElementById('paper');
 paperBtn.addEventListener('click', () => {
   console.log('Clicked paper');
   let result = (playRound('Paper', computerPlay()));
-  console.log(result);
 });
 
 const scissorsBtn = document.getElementById('scissors');
 scissorsBtn.addEventListener('click', () => {
   console.log('Clicked scissors');
   let result = (playRound('Scissors', computerPlay()));
-  console.log(result);
 });
